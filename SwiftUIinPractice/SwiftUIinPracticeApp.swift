84c365ffa61feaee1c99adbad72d5907ac736ef7
Yunus Olgun <y.olgun@gmail.com>
2025-04-10 14:52:37 +0300

    Initial Commit

---
+++ SwiftUIinPractice/SwiftUIinPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIinPracticeApp.swift
+//  SwiftUIinPractice
+//
+//  Created by yunus olgun on 10.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIinPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
