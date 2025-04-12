a830ae1e4974aa7ae5ea732331b9cfe074a775c5
Yunus Olgun <y.olgun@gmail.com>
2025-04-12 17:24:42 +0300

    Rebuild Bumble in SwiftUI (Part 1/5)

---
+++ SwiftUIinPractice/Bumble/Components/BumbleFilterView.swift
@@ -0,0 +1,58 @@
+//
+//  BumbleFilterView.swift
+//  SwiftUIinPractice
+//
+//  Created by yunus olgun on 12.04.2025.
+//
+
+import SwiftUI
+
+struct BumbleFilterView: View {
+    
+    var options: [String] = ["Everyone", "Trending"]
+    @Binding var selection: String
+    @Namespace private var namespace
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 32) {
+            ForEach(options, id: \.self) { option in
+                VStack(spacing: 8) {
+                    Text(option)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                    
+                    if selection == option {
+                        RoundedRectangle(cornerRadius: 2)
+                            .frame(height: 1.5)
+                            .matchedGeometryEffect(id: "selection", in: namespace)
+                    }
+                    
+                    
+                }
+                .padding(.top, 8)
+                .background(Color.black.opacity(0.001))
+                .foregroundStyle(selection == option ? .bumbleBlack: .bumbleGray)
+                .onTapGesture {
+                    selection = option
+                }
+            }
+        }
+        .animation(.smooth, value: selection)
+    }
+}
+
+fileprivate struct BumbleFilterViewPreview: View {
+    
+    var options: [String] = ["Everyone", "Trending", "Hello"]
+    @State private var selection: String = "Everyone"
+    
+    var body: some View {
+        BumbleFilterView(options: options, selection: $selection)
+    }
+}
+
+#Preview {
+    BumbleFilterViewPreview()
+        .padding()
+}
